f2bbf4c843f4299156ac88fce6cfe0cc814a6575
Silva2122 <56794281+Silva2122@users.noreply.github.com>
2019-10-29 18:14:30 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
+# Теперь за нас переменные окружения подгрузит докер.
 
 TOKEN = os.getenv("TOKEN")
 admin_id = os.getenv("ADMIN_ID")
@@ -10,3 +8,5 @@ host = os.getenv("PGHOST")
 PG_USER = os.getenv("PG_USER")
 PG_PASS = os.getenv("PG_PASS")
 
+BOT_TOKEN = "979965252:AAEWz7sBJ8ftG9928AXD7LWSc9HqUHdfODE"  # Your token
+admin_id = 97568090
